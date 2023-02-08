ed297d8b35e1521920b719ac63e857e86db6977b
Sherwin V Evan <sherwinvevan@outlook.com>
2023-02-09 00:41:29 +0530

    Containers positioned

--- src/main.js
+++ src/main.js
@@ -1,26 +1,43 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import './main.css';
 
 export function QuotePage() {
-    const quotesList = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
-    'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    const quotesList = [
+        {quote : '"Many of life’s failures are people who did not realize how close they were to success when they gave up."', author : 'name1'},
+        {quote : 'Bones2', author : 'name2'},
+        {quote : 'Bones3', author : 'name3'},
+        {quote : 'Bones4', author : 'name4'},
+    ];
 
     function getRandomIndex() {
         return Math.floor(Math.random() * quotesList.length);
     }
-    
-    function getRandomQuote() {
-        return quotesList[getRandomIndex()];
-    }
 
-    const [selectedQuote, setSelectedQuote] = useState(() => getRandomQuote());
+    let index = getRandomIndex();
+
+    const [selectedQuote, setSelectedQuote] = useState(quotesList[index].quote);
+    const [selectedAuthor, setSelectedAuthor] = useState(quotesList[index].author);
+
     function HandleRandomise() {
-        setSelectedQuote(getRandomQuote());
+        index = getRandomIndex();
+        setSelectedQuote(quotesList[index].quote);
+        setSelectedAuthor(quotesList[index].author);
     }
     return(
         <div className="bg">
-            {selectedQuote}
-            <br />
-            <button onClick={HandleRandomise}>Randomise</button>
+            <div className="quote-box">
+                <div className="text">
+                    {selectedQuote}
+                </div>
+                <div className="author">
+                    - {selectedAuthor}
+                </div>
+                <div className="new-quote">
+                    <button className="random-button" onClick={HandleRandomise}>Randomise</button>
+                </div>
+            </div>
         </div>
     )
 }
